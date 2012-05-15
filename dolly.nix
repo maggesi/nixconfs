706bc807b491db63311d2d53ed81c772d3a6bef4
maggesi <maggesi@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2012-05-15 10:06:27 +0000

    Reformatting dolly config
    git-svn-id: https://nixos.org/repos/nix/configurations/trunk/misc/z77z@34101 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- dolly.nix
+++ dolly.nix
@@ -3,64 +3,51 @@
 {
   require = [ "${modulesPath}/virtualisation/xen-domU.nix" ];
 
-  fileSystems = 
-    [ { mountPoint = "/";
-        label = "nixos";
-      }
-    ];
+  fileSystems = [ { mountPoint = "/"; label = "nixos"; } ];
 
-  networking = {
-    hostName = "dolly";
-    nameservers = [ "150.217.34.1"  "150.217.34.211" ];
-    defaultMailServer.directDelivery = true;
-    defaultMailServer.hostName = "mail.math.unifi.it";
+  networking =
+    { hostName = "dolly";
+      nameservers = [ "150.217.34.1" "150.217.34.211" ];
+      defaultMailServer.directDelivery = true;
+      defaultMailServer.hostName = "mail.math.unifi.it";
+      
+      useDHCP = false;
+      interfaces =
+        [ { name = "eth0";
+            ipAddress = "150.217.34.130";
+            subnetMask = "255.255.255.128";
+        } ];
+      defaultGateway = "150.217.34.129";
+    };
 
-    useDHCP = false;
-    interfaces = [ {
-      name = "eth0";
-      ipAddress = "150.217.34.130";
-      subnetMask = "255.255.255.128";
-    } ];
-    defaultGateway = "150.217.34.129";
-
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      emacs screen mosh
+  environment.systemPackages =
+    with pkgs;
+    [ emacs screen mosh
       mercurial darcs git fossil
-      ocaml
-      coq
-    ];
-  };
+      ocaml coq ];
 
   services.locate.enable = true;
   services.locate.period = "40 3 * * *";
   services.openssh.enable = true;
 
+  services.openafsClient.enable = false;
+  services.openafsClient.cellName = "math.unifi.it";
 
-
-
-  services.openafsClient = {
-      enable = false;
-      cellName = "math.unifi.it";
-    };
   services.postgresql.enable = true;
 
-  services.httpd = {
-      enable = true;
+  services.httpd =
+    { enable = true;
       hostName = "localhost";
       adminAddr = "maggesi@math.unifi.it";
       documentRoot = "/var/www";
       enableUserDir = true;
       /*
-      servedDirs = [
-        { dir = "/home/maggesi/Devel"; urlPath = "/devo"; }
-        { dir = "/nix/store"; urlPath = "/store"; }
-      ];
-      extraModules = [
-        { name = "php5"; path = "${pkgs.php}/modules/libphp5.so"; }
-      ];
+      servedDirs =
+        [ { dir = "/home/maggesi/Devel"; urlPath = "/devo"; }
+          { dir = "/nix/store"; urlPath = "/store"; } ];
+      extraModules =
+        [ { name = "php5";
+            path = "${pkgs.php}/modules/libphp5.so"; } ];
       extraConfig = ''
         ScriptAlias /cgi-bin/ /var/www/cgi-bin/
       '';
@@ -73,6 +60,4 @@
   krb5.defaultRealm = "MATH.UNIFI.IT";
   krb5.kdc = "kerberos.math.unifi.it";
   krb5.kerberosAdminServer = "kerberos.math.unifi.it";
-
-
 }
