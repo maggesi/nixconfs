bc0decd5274819021272dfcea8f8af327e042d96
maggesi <maggesi@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2012-05-18 20:21:43 +0000

    z77z configs: update release.nix and include missing files
    git-svn-id: https://nixos.org/repos/nix/configurations/trunk/misc/z77z@34177 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- release.nix
+++ release.nix
@@ -1,14 +1,17 @@
-{ nixpkgs ? <nixpkgs>
-, nixos ? <nixos>
-, configuration ? <configuration>
+{ nixpkgsSrc ? <nixpkgs>
+, nixosSrc ? <nixos>
 }:
 
 let
-  pkgs = import nixpkgs { };
-  eval = import (nixos + "/lib/eval-config.nix") {
-    inherit pkgs;
-    modules = [ configuration ];
-  };
-in {
-  system = eval.config.system.build.toplevel;
+  system = "i686-linux";
+  pkgs = import nixpkgsSrc { inherit system; };
+  eval = conf: import (nixosSrc + "/lib/eval-config.nix")
+    { inherit pkgs system;
+      modules = [ conf ];
+    };
+in
+{ 
+  elio = (eval ./elio.nix).config.system.build.toplevel;
+  neve = (eval ./neve/configuration.nix).config.system.build.toplevel;
+  virtux = (eval ./virtux.nix).config.system.build.toplevel;
 }
