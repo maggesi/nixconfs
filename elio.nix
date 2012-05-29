716ca88537f9ea73a4ff515d1696dc4bbe3562fb
maggesi <maggesi@70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb>
2012-05-29 12:13:06 +0000

    Update elio configuration.  Fixed the configuration of apache.
    git-svn-id: https://nixos.org/repos/nix/configurations/trunk/misc/z77z@34271 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- elio.nix
+++ elio.nix
@@ -3,6 +3,52 @@
 let
   hydrapkg = /nix/store/v70gk4mjqkyp2l4k73pxrbixinvzzsxr-hydra-0.1pre1051-4ad8912;
   #hydrapkg = /nix/store/2a49h1zc3cydy97dyrv3ycfia087wwcy-hydra-0.1pre1058-fdf441a;
+
+  nixosVHostConfig = {
+    hostName = "elio.math.unifi.it";
+    adminAddr = "maggesi@math.unifi.it";
+    documentRoot = "/var/www";
+    enableUserDir = true;
+    logFormat = ''"%h %l %u %t \"%r\" %>s %b %D"'';
+
+/*
+    servedDirs = [
+      { urlPath = "/tarballs";
+        dir = "/data/webserver/tarballs";
+      }
+    ];
+
+    servedFiles = [
+      { urlPath = "/releases/css/releases.css";
+        file = releasesCSS;
+      }
+    ];
+*/
+
+      extraConfig = ''
+        TimeOut 900
+
+        <Proxy *>
+          Order deny,allow
+          Allow from all
+        </Proxy>
+
+        ProxyRequests     Off
+        ProxyPreserveHost On
+        ProxyPass         /  http://localhost:3000/ retry=5 disablereuse=on
+        ProxyPassReverse  /  http://localhost:3000/
+
+        <Location />
+          SetOutputFilter DEFLATE
+          BrowserMatch ^Mozilla/4\.0[678] no-gzip\
+          BrowserMatch \bMSI[E] !no-gzip !gzip-only-text/html
+          SetEnvIfNoCase Request_URI \.(?:gif|jpe?g|png)$ no-gzip dont-vary
+          SetEnvIfNoCase Request_URI /api/ no-gzip dont-vary
+          SetEnvIfNoCase Request_URI /download/ no-gzip dont-vary
+        </Location>
+      '';
+    };
+
 in
 {
   require = [
@@ -12,14 +58,14 @@ in
 
   fileSystems = [ { mountPoint = "/"; label = "nixos"; } ];
   swapDevices = [ { device = "/dev/xvda1"; } ];
-  
+
   networking = {
     hostName = "elio";
     domain = "math.unifi.it";
     nameservers = [ "150.217.34.129" "150.217.34.211" ];
     defaultMailServer.directDelivery = true;
     defaultMailServer.hostName = "mail.math.unifi.it";
-      
+
     useDHCP = false;
     interfaces =
       [ { name = "eth0";
@@ -70,15 +116,9 @@ in
 
   services.httpd = {
     enable = true;
-    hostName = "elio.math.unifi.it";
+    logPerVirtualHost = true;
     adminAddr = "maggesi@math.unifi.it";
-    documentRoot = "/var/www";
-
-    #enableSSL = true;
-    #sslServerCert = "/root/ssl-secrets/elio.crt";
-    #sslServerKey = "/root/ssl-secrets/elio.key";
-
-    enableUserDir = true;
+    hostName = "localhost";
 
     extraModules = [
       #### Questi non servono, sono caricaty per default
@@ -86,10 +126,23 @@ in
     ];
 
     extraConfig = ''
-        ProxyPreserveHost on
-        RewriteEngine on
-        RewriteRule ^/(.*) http://localhost:3000/$1 [P,L] 
-     ''; 
+       AddType application/nix-package .nixpkg
+    '';
+
+    virtualHosts = [
+      nixosVHostConfig
+
+      (nixosVHostConfig // {
+        enableSSL = true;
+        sslServerCert = "/root/ssl-secrets/elio.crt";
+        sslServerKey = "/root/ssl-secrets/elio.key";
+	extraConfig = nixosVHostConfig.extraConfig + ''
+          # Required by Catalyst.
+          RequestHeader set X-Forwarded-Port 443
+        '';
+      })
+
+     ];
   };
 
   time.timeZone = "Europe/Rome";
