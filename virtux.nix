1c7266a07d684cbb7ebe487282bd77970ad3f806
maggesi@math.unifi.it <Marco Maggesi>
2013-03-24 12:53:42 +0100

    virtux non usa piu' useVirtualboxVideo

--- virtux.nix
+++ virtux.nix
@@ -5,7 +5,6 @@
 {pkgs, config, ...}:
 
 let
-  useVirtualboxVideo = true;
   texLivePaths = with pkgs; [
     texLive texLiveExtra texLiveCMSuper
     texLiveBeamer lmodern texLiveContext
@@ -40,7 +39,7 @@ in
   nix.maxJobs = 1;
 
   services = {
-    virtualbox.enable = useVirtualboxVideo;
+    virtualbox.enable = true;
 
     gpm.enable = true;
     ttyBackgrounds.enable = false;
@@ -65,26 +64,7 @@ in
       displayManager.auto = { enable = true; user = "maggesi"; };
 
       driSupport = true;
-
-    } // (if useVirtualboxVideo then {
-      videoDriver = "virtualbox";
-    } else {
-      videoDriver = "vesa";
-
-      # The following configuration can be used to get a convenient
-      # screen size when VirtualBox Guest Additions do not work.
-
-      resolutions = [ { x=1680; y=1050; } { x=1440; y=900; }
-                      { x=1280; y=1024; } { x=1152; y=864; }
-                      { x=1024; y=768; } { x=800; y=600; } ];
-
-      monitorSection = ''
-        HorizSync 24.0-92.0
-        VertRefresh 50.0-85.0
-        Modeline "1440x900" 106.5000 1440 1520 1672 1904 900 903 909 934 +HSync -VSync
-        Modeline "1680x1050" 188.1 1680 1800 1984 2288 1050 1051 1054 1096 +HSync -VSync
-      '';
-    });
+    };
   };
 
   environment = {
