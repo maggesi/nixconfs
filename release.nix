fcc57e00f384e76362cc396d990d2bf1a19b6aab
Marco Maggesi <maggesi@math.unifi.it>
2013-04-15 22:21:07 +0200

    Aggiunta holinux a release.nix

--- release.nix
+++ release.nix
@@ -17,4 +17,5 @@ in
   o0dom0 = (eval ./o0dom0/o0dom0-hydra.nix).config.system.build.toplevel;
   o0dom0_xen = (eval ./o0dom0/o0dom0-xen.nix).config.system.build.toplevel;
   virtux = (eval ./virtux/configuration.nix).config.system.build.toplevel;
+  holinux = (eval ./holinux/configuration.nix).config.system.build.toplevel;
 }
