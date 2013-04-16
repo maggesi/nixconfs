543d8dd0b1229e127b181fe2ef941281b62675d7
Marco Maggesi <maggesi@math.unifi.it>
2013-04-16 19:34:34 +0200

    Configurazione di elio in una directory separata

--- release.nix
+++ release.nix
@@ -12,7 +12,7 @@ let
 in
 { 
   dolly = (eval ./dolly.nix).config.system.build.toplevel;
-  elio = (eval ./elio.nix).config.system.build.toplevel;
+  elio = (eval ./elio/elio.nix).config.system.build.toplevel;
   neve = (eval ./neve/configuration.nix).config.system.build.toplevel;
   o0dom0 = (eval ./o0dom0/o0dom0-hydra.nix).config.system.build.toplevel;
   virtux = (eval ./virtux/configuration.nix).config.system.build.toplevel;
