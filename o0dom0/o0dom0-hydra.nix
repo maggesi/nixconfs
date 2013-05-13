edea9c3fb9a89a9b0e309cd96f11d50685b4c2fe
Marco Maggesi <maggesi@math.unifi.it>
2013-05-13 14:10:31 +0200

    Nuova versione di hydra.

--- o0dom0/o0dom0-hydra.nix
+++ o0dom0/o0dom0-hydra.nix
@@ -4,9 +4,10 @@
 { config, pkgs, ... }:
 
 let
-  # hydrapkg = /nix/store/7vlizjmz662s6pifyh596n3b5zacy7iv-hydra-0.1pre1271-9f027b2;
   # hydrapkg = /nix/store/chdvwyw98yj1w2nny3mgh40c37x5cp2k-hydra-0.1pre1271-9f027b2;
-  hydrapkg = /nix/store/4fqr3km5hrkd7np3mwaav3s1ryrz80a0-hydra-0.1pre1312-f447c7d;
+  # hydrapkg = /nix/store/4fqr3km5hrkd7np3mwaav3s1ryrz80a0-hydra-0.1pre1312-f447c7d;
+  hydrapkg = /nix/store/18wp32aahmizhbws25yxhq1j37bdwh9m-hydra-0.1pre1327-43785df;
+  # hydrapkg = /root/.nix-profile;
 
   nixosVHostConfig = {
     hostName = "o0dom0.math.unifi.it";
