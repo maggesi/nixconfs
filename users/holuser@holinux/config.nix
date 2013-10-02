dbf4ea4de2967b337da522ee04dc133fef2c4f0c
Marco Maggesi <maggesi@math.unifi.it>
2013-10-02 21:56:45 +0200

    Aggiornata la configurazione di holuser@holinux

---
+++ users/holuser@holinux/config.nix
@@ -0,0 +1,30 @@
+# ~/.nixpkgs/config.nix
+{
+  packageOverrides = pkgs: rec {
+    emacsEnv = pkgs.buildEnv
+      { name = "emacs-env";
+        paths = with pkgs.emacs24Packages;
+          [ emacs
+            #proofgeneral
+            #hol_light_mode
+          ];
+      };
+
+    miscEnv = pkgs.buildEnv
+      { name = "misc-env";
+        paths = with pkgs; [
+          coreutils diffutils findutils file which
+        ];
+      };
+
+    hol_light = with pkgs; import ../../../pkgs/hol_light {
+      inherit stdenv fetchsvn writeScript;
+      inherit (ocamlPackages) ocaml findlib;
+      camlp5 = ocamlPackages.camlp5_strict;
+    };
+
+    holl_mode = with pkgs; import ../../../pkgs/holl-mode {
+      inherit stdenv fetchurl emacs;
+    };
+  };
+}
