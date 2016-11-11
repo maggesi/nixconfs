0d5da21392cd2118d5ee77f6d24db822ebeac170
Marco Maggesi <maggesi@math.unifi.it>
2016-11-11 16:51:45 +0100

    Update elio config.

--- systems/elio/elio.nix
+++ systems/elio/elio.nix
@@ -37,6 +37,9 @@ in {
     defaultMailServer.directDelivery = true;
     defaultMailServer.hostName = "mail.math.unifi.it";
 
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [ 80 443 5000 ];
+
     useDHCP = false;
     interfaces =
       [ { name = "eth0";
@@ -73,10 +76,27 @@ in {
   #services.httpd.documentRoot = "/home/maggesi/public_html";
 
   services.lighttpd.enable = true;
+  services.lighttpd.port = 443;
+  services.lighttpd.enableModules = [ "mod_auth" ];
+  services.lighttpd.extraConfig = ''
+      ssl.engine = "enable"
+      ssl.pemfile = "/srv/certs/lighttpd.pem"
+      auth.backend = "htdigest"
+      auth.backend.htdigest.userfile = "/srv/htdigest"
+      auth.require = (
+          "/cgit" => (
+              "method"  => "digest",
+              "realm"   => "cgit interface",
+              "require" => "valid-user"
+          )
+      )
+    '';
   services.lighttpd.cgit.enable = true;
   services.lighttpd.cgit.configText = ''
       cache-size=1000
-      scan-path=/srv/cgit
+      snapshots=tar.gz tar.bz2 tar.xz zip
+      section-from-path=1
+      scan-path=/home/maggesi/Repos/Git
     '';
 
   #services.openafsClient.enable = true;
@@ -105,7 +125,15 @@ in {
 
   nix.sshServe.enable = true;
   nix.sshServe.keys = [
-    "b+J7JbwWK3crIUe6mktC7uFZa+NOX/fVynpDP/WDaG7SnS9T6my+yg4tSB/Y2k6irRxo9oakgM224hLYTaL46CweHHLxEI0AAXhzi/pPKnRNhjX7lt1TE6a+xnuWCOeKe/wEnCpSg33VkcWWFi5h7tYOeHyzYYlRMS3VWVbrWGmb5WXRfYRwTEoxQ2FDBrYWx9+Gt+vBh maggesi@neve"
+    # root@crystal
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/2HrwB09EfRWQ4le+cJTihrmsz/4roGxJA4+IrGF/KmWu8+02xdxebF94wHRO1cCLAUMaZV7RBQu5MNjsCB8rALqU+D0fd9ywqbhapp7fSp9UQcgcoMdonzA+HvbJ0w/b9wJF3FNfkS0Zp4PeLztGsToW47iGH3VpioTA0g3NFuJjKY0PYIFH0zBDMjCE7hMoALAf26n55W1yCK1gpfdNrThtcyIkmaqQZHyhP8PGwudJ4y2xNk1iyFcs6XsGrSmQzCuMIXQXjOA3atoCRFOPwogYy0itr8EICR/4ZgSXBWoMX2OFPktRz2GwclPhCgnEqRYyMPVA1mXVpLRTRTb7 root@crystal"
+    # maggesi@crystal
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDj6Fy0LwW1Cj1BOf9Zk0k4KLi0dNCYMjucik8K3ts5UONwUby/eeOzegvM6n1bCPERy5O5Pj9RilnPCdA9MN5dxLK2zUkyQEp4/EpgeYk03H1QrI9PSqdj8zvtKDItM/pdcLhOelvldBzILsbZ4f60HRb08QmiX8o31HsU0e1+6cRjKXP4UfAg1qw1JuJdO/pY/TlsAOxdTFidKX4GYcqjVSExF91LKg1511jfM9ZG5RlenahQ3b5wXttykZjSveO5uzMi/daEj5ALGzzZ7ItOKBwIehlFyEz6sqoagS5weh0YF6F/VL6BW9LybHxsCX7ZqoUpJcdjyGWUDn7sven3 marco.maggesi@gmail.com"
+    # root@neve
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCBuU6zkmy87QOFtMnvwkKt0PPa0rTtg9WInVwyPc2Y6XtuxxQSaovw25jJz9FjLTqw5AnzD84M4CrEL1CaRh5u+5ntV5qrqslmDpDu8ourmpbNcL2dbGhuN4qdnAuYr391OsQ2z5OTZZUBzUrT1e4gg/U6kGfpb0L45UiWHhsB94ep8oNA+bwOx446zd3k/OBVU0f/U4DwS1Jym6J91U3hP2Y9WVaBmWmkk6BMVN548ZXKkI6mcCU0uMltYOwkOt4E+GHezdCYE0bfc/xoSsarBYYCbkPVzFiiXmaiRvavnN+msOB2vP8KzpQdC7jHkf9IwOXAwoK+yIEWaRjrQGt root@neve"
+    # maggesi@neve
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDa8b0cgizsaZKszLd701sByujgArnhkVoGgTYGJEZRt0/8qjo96eiJykxqcneXByigeM0qosyhUXY3SSXyUK0plvjROKIzojYmuiNfR2yr/pTdP/BraFx8DU8YoEyrwj3bS3GX4nTY+cng+wa4o81ZxwENg5Q1QXsB0GVn51OTsHkbVQSNs+tR5WcxLo0fVUeb1K+HJkz1j7LDT33IZg6iuxlD1NGouojwBurTo2VdrSxPqA/S3OhP2vsjn5YYnUX0avHvnQ44lWMxipxAzJHa6iX6X/fQyDSy6PGHQnCCGqI7oXtp0HQajCs+cKWuFu3lfnOTEBAQnWljUSWyHhGL maggesi@neve"
+
     "cVhiJJkNqVhgrAzEOmENGakmaA74sPvNCDRs/xqMzto1Y7jnb7QAnjlbCOSHo/B7Qrq41qTrUZWgQJZ/LOwjNXNpR2usECyyA+zmRK6mWNvXk20JKc29CIptv4Bpq8J+CrF6PRYmgZvWlP41ndXj9tUabZUNYPoDrFqGYt7aOaMG67dQhV2j8NwT/4c4GRSz5ImeUkb/t maggesi@bantu"
     "1bZJHO3zY4nqJlU2YEI5GJX3xW8JjjTzE+BcpuObElS1Z0A9E+GpmVy3l+Y8cJIu8+CPPWLJKFxtNe3CMsaRUbgyPOrBuzmcGZbXOGApiANKNTBYaUqZgAzN7aX2k1H0GifwieO7q5dFfCrXffuKrSP5O2Uis7bS5xsGbetXUWticP05D/G5keFoXgSxEjJ3a1YopMFJv maggesi@virtux"
     "9ol2wqsO3MG44VUyOIozJDe/ak0kzfVS6gxoYDym7ZRWJ0XEqnCFlK+mBMHx2FL9Q/LNpuNmFZTPufScimgPvWmmq85odMuvwEUVEWTrv9neDaIspU7va3nRf79SYIbQoHFg97HsOGnISxCqiw87MzcetYU68khVcVUKUobyP2BT5zUywJmB1zVZL5ZFdBml5f52knw== marco.maggesi@gmail.com"
